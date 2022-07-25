707a329ad0969ee62647c2c29b8e2e32e29303bd
React <nk.namirkhan1@gmail.com>
2022-07-25 15:40:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+const App = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
 
-function App() {
+  const [allentry, setallentry] = useState([]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      const newentry = { id: new Date().getTime().toString(), email, password };
+
+      setallentry([...allentry, newentry]);
+      setemail("");
+      setpassword("");
+    } else {
+      alert("please fill the following details");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <form action="" onSubmit={submitForm}>
+        <div className="main">
+          <div>
+            <label htmlFor="email">
+              Email
+              <br />
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+          </div>
 
+          <div>
+            <label htmlFor="password">
+              Password
+              <br />
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <div className="showdetail">
+        {allentry.map((currElm) => {
+          return (
+            <div key={currElm.id} className="show detail">
+              <p>{currElm.email}</p>
+              <p>{currElm.password}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 export default App;
